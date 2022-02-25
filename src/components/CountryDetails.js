62e75baecb0fe0867488e48568dbe3cb302aa4d4
Eliza <seeubanks12@gmail.com>
2022-02-25 16:21:23 -0500

    done

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,38 +1,41 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import countriesArr from '../countries.json';
+// import countriesArr from '../countries.json';
 import axios from 'axios';
 
 const CountryDetails = () => {
-  const [foundCountry, setfoundCountry] = React.useState([]);
+  const [foundCountry, setFoundCountry] = React.useState(null);
 
+  const { alpha3Code } = useParams();
   React.useEffect(() => {
     axios
-      .get(`https://ih-countries-api.herokuapp.com/countries`)
+      .get(`https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`)
       .then((res) => {
-        const foundCountry = res.data.alpha3Code;
-        setFoundCountry(foundCountry);
+        const newFoundCountry = res.data;
+        setFoundCountry(newFoundCountry);
+        console.log(foundCountry);
       });
-  }, []);
+  }, [alpha3Code]);
 
-//   const { alpha3Code } = useParams();
+  //   let foundCountry = countriesArr.find((country) => {
+  //     return country.alpha3Code === alpha3Code;
+  //   });
 
-// //   let foundCountry = countriesArr.find((country) => {
-// //     return country.alpha3Code === alpha3Code;
-// //   });
-
-//   return (
-//     <div className="details">
-//       <h3>{foundCountry.name.official}</h3>
-//       <p>{foundCountry.capital}</p>
-//       <p>{foundCountry.area}</p>
-//       <p>
-//         {foundCountry.borders.map((border) => {
-//           return <li>{border}</li>;
-//         })}
-//       </p>
-//     </div>
-//   );
+  return foundCountry ? (
+    <div className="details">
+      <h3>{foundCountry.name.official}</h3>
+      <p>{foundCountry.capital}</p>
+      <p>{foundCountry.area}</p>
+      <p>
+        {foundCountry.borders.map((border) => {
+          return <li>{border}</li>;
+        })}
+      </p>
+    </div>
+  ) : (
+    <div>
+      <h1>Loading....</h1>
+    </div>
+  );
 };
-
 export default CountryDetails;
